0b3a81b2be1f68e3a40f4487428ef4e575976447
Shivam Sinha <shivam.sinha@Shivam-Sinha-LVHLXYD3HX.local>
2025-03-29 18:10:33 +0530

    parcel hosting

--- App.js
+++ App.js
@@ -2,7 +2,21 @@
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(heading);
 
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", {id:"parent"}, React.createElement("div", {id:"child"}, [React.createElement("h1", {}, "Hello world from reactjs"),React.createElement("h2", {}, "Hello world from reactjs")]))
+
+const parent = React.createElement(
+    "div",
+    { id: "parent" },
+    React.createElement(
+      "div",
+      { id: "child" },
+      [
+        React.createElement("h1", { key: "h1" }, "Hello world from ReactJS"),
+        React.createElement("h2", { key: "h2" }, "Hello world from ReactJS"),
+      ]
+    )
+  );
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
